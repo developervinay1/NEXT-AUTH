a37e1f6421586b5b9092b23a960c847759b28231
developer-vsandhu <77821789+developervinay1@users.noreply.github.com>
2023-08-23 09:56:20 +0530

    XML FILES

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,6 @@ import Navbar from "@/components/Navbar";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import { NextAuthProvider } from "./Provider";
-import { SessionProvider } from "next-auth/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
